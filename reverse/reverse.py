0a020888659ba1e8a56b66468a67440fad8bee46
Jisha Obukwelu <jisha@obukwelu.com>
2020-09-28 09:02:43 -0400

    Trying to print reversed list

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -38,8 +38,9 @@ class LinkedList:
 
         return False
 
-    def reverse_list(self, node, prev):
+    def reverse_list(self, node=None, prev=None):
         current_node = self.head
+        print("Current Node",current_node.get_value())
 
         # create list to hold nodes as they get removed
         stack = []
@@ -48,15 +49,35 @@ class LinkedList:
         while current_node != None:
             stack.append(current_node.get_value())
             current_node = current_node.get_next()
+            print("Stack", stack)
 
         # Using LIFO from stack
         if len(stack) > 0:
             self.head = Node(stack.pop())
             current_node = self.head
+            print("Reversed List",current_node.get_value())
 
             while len(stack) > 0:
                 current_node.set_next(Node(stack.pop()))
                 current_node = current_node.get_next()
+                print("Reversed",current_node.get_value())
+
+    # another methode
+    def reverse(self, curr_node=None, prev=None):
+            prev = None
+            curr_node = self.head
+            while(curr_node is not None):
+                next = curr_node.next_node
+                curr_node.next_node = prev
+                prev = curr_node
+                curr_node = next
+            self.head = prev
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.get_value())
+            temp = temp.get_next()
 
 """
 ......
@@ -73,6 +94,11 @@ if __name__ == "__main__":
     ll.add_to_head(2)
     ll.add_to_head(10)
 
-    print(ll.reverse_list(ll.head, None))
+    print(ll.printList())
+    ll_r = ll.reverse()
+    ll_r2 = ll.reverse_list()
+    # print(ll_r.printList())
+    # print(ll_r2.printList())
+
     #unsure how to test
     # tests run but reverse_list returns None
